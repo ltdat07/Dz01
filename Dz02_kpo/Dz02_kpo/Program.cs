892e93c8f8958e064ad6b81592d76c895e3eb06b
ltdat07 <danielle172004@gmail.com>
2025-01-26 23:09:43 +0300

    Update Program.cs

--- Dz02_kpo/Dz02_kpo/Program.cs
+++ Dz02_kpo/Dz02_kpo/Program.cs
@@ -15,29 +15,24 @@ class Program
         PedalCarFactory pedalFactory = new PedalCarFactory();
         HandCarFactory handFactory = new HandCarFactory();
 
-        // Добавляем покупателей
         customerStorage.AddCustomer(new Customer("Alicia", 6, 4));
         customerStorage.AddCustomer(new Customer("Bobop", 4, 6));
         customerStorage.AddCustomer(new Customer("MJ", 6, 6));
         customerStorage.AddCustomer(new Customer("Dav", 4, 4));
 
-        // Добавляем автомобили
-        carService.AddCar(pedalFactory, new PedalEngineParams { PedalSize = "Large" });
-        carService.AddCar(pedalFactory, new PedalEngineParams { PedalSize = "Medium" });
+        carService.AddCar(pedalFactory, new PedalEngineParams { PedalSize = "Bolshoi" });
+        carService.AddCar(pedalFactory, new PedalEngineParams { PedalSize = "Sredni" });
         carService.AddCar(handFactory, EmptyEngineParams.DEFAULT);
         carService.AddCar(handFactory, EmptyEngineParams.DEFAULT);
 
-        // Вывод информации о покупателях и их автомобилях
         Console.WriteLine("Initial Customers State:");
         foreach (var customer in customerStorage.GetCustomers())
         {
             Console.WriteLine(customer);
         }
 
-        // Реализация процесса продажи автомобилей
         hseCarService.SellCars();
 
-        // Вывод информации о покупателях и их автомобилях после продажи
         Console.WriteLine("\nFinal Customers State:");
         foreach (var customer in customerStorage.GetCustomers())
         {
